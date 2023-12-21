9404547496bf91a0f52650ee3f42e285fda61178
jai613 <110447410+jai613@users.noreply.github.com>
2023-12-21 11:17:13 +0530

    Committing files

---
+++ CodeForces/Python 3/1853A | Desorting/215382118.py
@@ -0,0 +1,20 @@
+import math
+ 
+for _ in range(int(input())):
+ 
+    n=int(input())
+    arr=[int(i) for i in input().split()]
+    if sorted(arr)!=arr:
+        print(0)
+        continue
+    if len(set(arr))==1:
+        print(1)
+        continue
+    else:
+        x=float('inf')
+        onee=0
+        twoo=0
+        k=float('inf')
+        for i in range(n-1):
+            k=min(abs(arr[i+1]-arr[i]),k)
+        print(math.ceil(k//2)+1)  
